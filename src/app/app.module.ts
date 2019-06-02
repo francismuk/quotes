cea461180daf498f1c431441988ce59e46756910
Francis Mukuha <mukuha58@gmail.com>
2019-06-02 22:25:36 +0300

    edit highlight directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightDirective } from './highlight.directive';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    HighlightDirective
+    HighlightDirective,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule
--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input , HostListener } from '@angular/core';
+import {Quote} from './quote';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input() quote:Quote;
 
 }
