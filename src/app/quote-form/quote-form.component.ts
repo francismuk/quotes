2462a7ef81e225ecc0499a57f26fb762e9f7b28a
Francis Mukuha <mukuha58@gmail.com>
2019-06-02 22:57:09 +0300

    add submit form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+export class GoalFormComponent implements OnInit {
+    newGoal=new Goal(0,"","",new Date());
+    @Output() addGoal=new EventEmitter<Goal>();
 
-@Component({
-  selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
-})
-export class QuoteFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    submitQuote(){
+        this.addQuote.emit(this.newGoal);
+    }
 }
