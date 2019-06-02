825399ca3af9742a96c09564105dfcb1f964c672
Francis Mukuha <mukuha58@gmail.com>
2019-06-02 22:19:41 +0300

    add highlight directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import {Quote} from './quote'
 })
 export class AppComponent {
   quotes = [
-      new Quote(1,'Watch Finding Nemo'),
-      new Quote(2,'Buy Cookies'),
-      new Quote(3, 'Get new Phone Case'),
-      new Quote(4, 'Get Dog Food'),
-      new Quote(5, 'Solve math homework'),
-      new Quote(6, 'Plot my world domination plan'),
+      new Quote(1,'Watch Finding Nemo', 'ss'),
+      new Quote(2,'Buy Cookies', 'ss'),
+      new Quote(3, 'Get new Phone Case', 'ss'),
+      new Quote(4, 'Get Dog Food', 'ss'),
+      new Quote(5, 'Solve math homework', 'ss'),
+      new Quote(6, 'Plot my world domination plan', 'ss'),
 
   ]
 }
