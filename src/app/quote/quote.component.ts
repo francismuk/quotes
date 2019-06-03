c2395e1ec87ba023add6860b9e4ca0937457c7f5
Francis Mukuha <mukuha58@gmail.com>
2019-06-03 09:59:46 +0300

    add upvote and downvote on examples

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,14 +9,14 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
     quotes = [
-        new Quote(1, '“The Way Get Started Is To Quit Talking And Begin Doing.”','By James Mwai', 'Walt Disney','10', '2'),
+        new Quote(1, '“The Way Get Started Is To Quit Talking And Begin Doing.”','By James Mwai', 'Walt Disney',1, 2),
         new Quote(2, '“Don’t Let Yesterday Take Up Too Much Of Today.”','By James Mwai', 'Will Rogers',  '10', '2'),
-        new Quote(3, '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”', 'Vince Lombardi', 'By James Mwai',  '10', '2'),
+        new Quote(3, '“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”', 'Vince Lombardi', 'By James Mwai',  10, 2),
 // tslint:disable-next-line: max-line-length
-        new Quote(4, '“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”','By James Mwai', 'Rob Siltanen', '10', '2'),
-        new Quote(5, '“We May Encounter Many Defeats But We Must Not Be Defeated.”','By James Mwai', 'Maya Angelou', '10', '2'),
-// tslint:disable-next-line: max-line-length
-        new Quote(6, '“Imagine Your Life Is Perfect In Every Respect; What Would It Look Like?”','By James Mwai', 'Brian Tracy',  '10', '2'),
+        new Quote(4, '“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”','By James Mwai', 'Rob Siltanen', 10, 2),
+        new Quote(5,'Coding is the epitome if fun', 'Virginia X', 'Admin', 0, 0),
+        // tslint:disable-next-line: max-line-length
+        new Quote(6, '“Imagine Your Life Is Perfect In Every Respect; What Would It Look Like?”','By James Mwai', 'Brian Tracy',  10, 2),
     ]
 
     deleteQuote(isComplete, index){
